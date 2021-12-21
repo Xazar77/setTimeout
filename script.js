98cda3d7b555e63e284bc3e44a750e2d2ded9f27
Anderey Prokhorov <beta737@yandex.ru>
2021-12-21 22:12:43 +0300

    lesson19 difficult

---
+++ script.js
@@ -0,0 +1,35 @@
+'usestrict';
+
+
+const input = document.querySelector('.input'),
+    text = document.querySelector('.text');
+
+/
+
+function debounce() {
+    input.addEventListener('change', () => {
+        text.textContent = "";
+        const showTextFunc = () => {
+            text.textContent = input.value;
+            input.value = "";
+        };
+        setTimeout(showTextFunc, 300);
+    });
+}
+debounce();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
